a7239a4dc94444d65f6d613b1f36accf17561c43
Akash Ramjyothi <akash.ramjyothi@gmail.com>
2024-08-27 20:39:36 +0530

    Fixed intendation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,8 +8,8 @@ export default function Header() {
       <h1>ReactArt</h1>
       <p style={{
         color:'red',
-        
         textAlign:'left'
+
       }}>A community of artists and art-lovers.</p>
     </header>
   );
