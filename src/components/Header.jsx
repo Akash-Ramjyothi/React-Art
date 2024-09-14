5599f7b524e76672784e2d819aaad432ded1bcbc
Akash Ramjyothi <akash.ramjyothi@gmail.com>
2024-09-14 22:23:35 +0530

    Initialized tailwind

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,7 @@ import logo from '../assets/logo.png';
 // Header function
 export default function Header() {
   return (
+    // Tailwind
     <header className='flex flex-col items-center mt-8 mb-16'>
       <img src={logo} alt="A canvas" className='mb-8 w-44 h-44 object-contain' />
       <h1 className='text-4xl font-semibold tracking-widest text-center uppercase text-amber-800 font-title'>
