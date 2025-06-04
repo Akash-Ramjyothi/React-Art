68b471f6ba1d2cb205cb96eec5df504bdb6c6f40
Akash Ramjyothi <aramjyothi@tradesun.com>
2025-06-04 23:07:39 +0530

    Formated code

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,3 +1,3 @@
-export default function Buttoon({children, ...props}){
+export default function Buttoon({ children, ...props }) {
   return <button {...props} className="px-4 py-2 font-semibold uppercase rounded text-stone-900 bg-amber-400 hover:bg-amber-500">{children}</button>
 }
